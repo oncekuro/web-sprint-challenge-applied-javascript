d0d463a5e96118450d97d7e31dc7e1ca77bc081e
oncekuro <oncekuro@users.noreply.github.com>
2020-10-09 19:39:46 -0700

    cards/header/tabs done, no stretch

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,49 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardEntry = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    console.log(response.data.articles);
+    for (let article in response.data.articles) {
+      response.data.articles[article].forEach((article) => {
+        cardEntry.appendChild(Card(article));
+      });
+    }
+  })
+  .catch((error) => {
+    console.log("Card Data Not Returned", error);
+  });
+
+function Card(data) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgDiv = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgDiv.classList.add("img-container");
+
+  img.src = data.authorPhoto;
+  headline.textContent = data.headline;
+  authorName.textContent = data.authorName;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgDiv);
+  imgDiv.appendChild(img);
+  author.appendChild(authorName);
+
+  card.addEventListener("click", () => {
+    console.log(data.headline);
+  });
+
+  return card;
+}
